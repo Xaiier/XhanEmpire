9ce3c6cd23362a44e0f3dc787dc83b4ab9f7834f
Xaiier <Xaiier@users.noreply.github.com>
2022-01-23 21:34:59 -0500

    psyblast charge improvements

--- src/data/scripts/weapons/XHAN_PsyBlastEveryFrame.java
+++ src/data/scripts/weapons/XHAN_PsyBlastEveryFrame.java
@@ -1,20 +1,20 @@
 package data.scripts.weapons;
 
-import com.fs.starfarer.api.combat.CombatEngineAPI;
-import com.fs.starfarer.api.combat.EveryFrameWeaponEffectPlugin;
-import com.fs.starfarer.api.combat.WeaponAPI;
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.combat.*;
 import com.fs.starfarer.api.util.IntervalUtil;
+import org.lazywizard.lazylib.MathUtils;
 import org.lazywizard.lazylib.VectorUtils;
 import org.lwjgl.util.vector.Vector2f;
 
 import java.awt.*;
 
-public class XHAN_PsyBlastEveryFrame  implements EveryFrameWeaponEffectPlugin {
+public class XHAN_PsyBlastEveryFrame implements EveryFrameWeaponEffectPlugin {
     private static final Color CHARGEUP_GLOW_COLOUR = new Color(213, 28, 255, 255);
     private static final float CHARGEUP_PARTICLE_DISTANCE_MIN = 250f;
     private static final float CHARGEUP_PARTICLE_DISTANCE_MAX = 250f;
     private static final float CHARGEUP_PARTICLE_VEL_MAX = 550f;
-    private static final float CHARGEUP_PARTICLE_SIZE_MAX= 25f;
+    private static final float CHARGEUP_PARTICLE_SIZE_MAX = 25f;
     private static final float CHARGEUP_PARTICLE_SIZE_MIN = 1f;
     private static final float CHARGEUP_PARTICLE_DURATION = 0.45f;
 
@@ -22,8 +22,17 @@ public class XHAN_PsyBlastEveryFrame  implements EveryFrameWeaponEffectPlugin {
     private static final Color MUZZLE_GLOW_COLOUR_EXTRA = new Color(235, 22, 114, 230);
     private static final float MUZZLE_GLOW_SIZE = 95f;
 
+    private static final float EMP_RATE = 5f; // 1/5
+    private static final float EMP_THICKNESS_SCALE = 10f;
+    private static final float JITTER_INTENSITY = 0.25f;
+    private static final float MAX_UNDER_JITTER_RANGE = 50f;
+
+    private static final String CHARGE_SOUND = "XHAN_PSYBLAST_CHARGE_SOUND";
+    private static final String EMP_SOUND = "system_emp_emitter_impact";
+
     private IntervalUtil particleInterval = new IntervalUtil(0.000001f, 0.000001f);
     private boolean hasFired = false;
+    private boolean hasCharged = false;
     private float oldChargeLevel = 0f;
     private float oldCooldown = 0f;
 
@@ -40,6 +49,11 @@ public class XHAN_PsyBlastEveryFrame  implements EveryFrameWeaponEffectPlugin {
 
         if (chargeLevel > oldChargeLevel || cooldown < oldCooldown) {
             if (weapon.isFiring()) {
+                if (!hasCharged) {
+                    Global.getSoundPlayer().playSound(CHARGE_SOUND, 1.0f, 1.0f, weapon.getLocation(), new Vector2f());
+                    hasCharged = true;
+                }
+
                 particleInterval.advance(amount);
 
                 if (particleInterval.intervalElapsed()) {
@@ -55,7 +69,40 @@ public class XHAN_PsyBlastEveryFrame  implements EveryFrameWeaponEffectPlugin {
 
                     float size = CHARGEUP_PARTICLE_SIZE_MIN + (chargeLevel * (CHARGEUP_PARTICLE_SIZE_MAX - CHARGEUP_PARTICLE_SIZE_MIN));
 
-                    engine.addSmoothParticle(loc, vel, size,10f, CHARGEUP_PARTICLE_DURATION, CHARGEUP_GLOW_COLOUR);
+                    engine.addSmoothParticle(loc, vel, size, 10f, CHARGEUP_PARTICLE_DURATION, CHARGEUP_GLOW_COLOUR);
+
+                    if (chargeLevel > oldChargeLevel) {
+                        /*
+                        engine.spawnEmpArcVisual(weapon.getLocation(),
+                                weapon.getShip(),
+                                MathUtils.getRandomPointInCircle(weapon.getLocation(), weapon.getShip().getShieldRadiusEvenIfNoShield()),
+                                weapon.getShip(),
+                                10f,
+                                MUZZLE_GLOW_COLOUR,
+                                Color.white
+                        );
+                        */
+                        if (MathUtils.getRandomNumberInRange(0f, EMP_RATE) < chargeLevel) {
+                            EmpArcEntityAPI e = engine.spawnEmpArcPierceShields(weapon.getShip(),
+                                    weapon.getShip().getAllWeapons().get(MathUtils.getRandomNumberInRange(0, weapon.getShip().getAllWeapons().size() - 1)).getLocation(),
+                                    weapon.getShip(),
+                                    weapon.getShip(),
+                                    DamageType.OTHER,
+                                    0f,
+                                    0f,
+                                    99999f,
+                                    EMP_SOUND,
+                                    chargeLevel * EMP_THICKNESS_SCALE,
+                                    MUZZLE_GLOW_COLOUR_EXTRA,
+                                    Color.white
+                            );
+                            e.setSingleFlickerMode();
+                        }
+
+                        float jitterRangeBonus = chargeLevel * MAX_UNDER_JITTER_RANGE;
+                        weapon.getShip().setJitterUnder(this, MUZZLE_GLOW_COLOUR, (float) Math.pow(chargeLevel, 2), 11, 0f, 3f + jitterRangeBonus);
+                        weapon.getShip().setJitter(this, MUZZLE_GLOW_COLOUR, chargeLevel * JITTER_INTENSITY, 11, 0f, 3f);
+                    }
                 }
             }
         }
@@ -68,6 +115,9 @@ public class XHAN_PsyBlastEveryFrame  implements EveryFrameWeaponEffectPlugin {
             hasFired = false;
         }
 
+        if (chargeLevel == oldChargeLevel && cooldown == oldCooldown) {
+            hasCharged = false;
+        }
 
         oldChargeLevel = chargeLevel;
         oldCooldown = cooldown;
